56cd53e1abf0693f98a169e2dd7c51f6fd2fe58c
nandinichhajed <nandinichhajed08@gmail.com>
2023-05-02 12:26:37 +0530

    generated threads successfully

--- chat/views.py
+++ chat/views.py
@@ -11,40 +11,34 @@ def chat(request):
         'chats': chats,
     })
 
+# create list for messages
+messages = []
+prompt = "A smart personal Assistant that is located in India"
+messages.append({"role": "system", "content": f"{prompt}"})
+
+
 @csrf_exempt
 def Ajax(request):
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-
         text = request.POST.get('text')
-        prompt = "A smart personal Assistant that is located in India"
         print(text)
 
         openai.api_key = os.environ['openai.api_key']
-        
-        # create list for messages
-        messages = []
-        messages.append({"role": "system", "content": f"{prompt}"})
-        
-        def CustomChatGPT():
-            messages.append({"role": "user", "content": f"{text}"})
-            res = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-            )
-        
-            response = res.choices[0].message["content"]
-            messages.append({"role": "assistant", "content": f"{response}"})
-            
-            chat = Chat.objects.create(
-            text = text,
-            gpt = response
+
+        messages.append({"role": "user", "content": f"{text}"})
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
         )
-            
-            print(messages)
-            # print(response)
-            return response
-        
 
+        response = res.choices[0].message["content"]
+        messages.append({"role": "assistant", "content": f"{response}"})
 
-        return JsonResponse({'data': CustomChatGPT(),})
+        chat = Chat.objects.create(
+            text=text,
+            gpt=response
+        )
+        print(messages)
+
+        return JsonResponse({'data': response})
     return JsonResponse({})
